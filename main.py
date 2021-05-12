64b3f59554e443099a9be4165a9af7409016e13d
Marcus <marcusvennerhagberg@gmail.com>
2021-05-12 02:11:58 +0200

    adding circular and random mode

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
 import random
 import time
+import math
 
+#MAX_HUE = 65534
 url = "http://192.168.0.144/api/hpWNy9jfbACROBTB8diURwZQabXua0KrGcFV3pRe"
+
+min_bri = 10
+max_bri = 254
+max_hue = 65535
+
+bri = 254
+
+lastBriHigh = True;
 
 data = """
 {
@@ -13,30 +23,65 @@ data = """
 # Set headers
 response = requests.get(url, headers = {"Content-Type": "application/json"})
 
-print(response)
+print("Setting header response: \n" + str(response))
 
 # Turn the light on
 response = requests.put(url + "/lights/1/state", data)
 
-while True:
+def setBri():
+	global lastBriHigh
+	global bri
+	if (lastBriHigh):
+		lastBriHigh = not lastBriHigh
+		bri = min_bri
+	else:
+		lastBriHigh = not lastBriHigh
+		bri = max_bri
 
-	time.sleep(1)
+def randomHue():
+	n = 0
 
-	x = random.uniform(0,1)
-	y = random.uniform(0,1)
+	while True:
 
-	print("x: " + str(x) + "\t" + "y: " + str(y))
+		time.sleep(0.2)
 
-	data = """
-	{
-		"on": true,
-		"xy": [%f, %f]
-	}""" % (x, y)
+		#Clamp between 0 and 1
+		hue = int(random.random() * max_hue)
 
-	response = requests.put(url + "/lights/1/state", data)
- 
-# Send data
-#response = requests.put(url + "/lights/1/state", data)
+		if (n%2 == 0):
+			setBri()
 
-#print(response)
-#print(response.content)
+		data = """
+		{
+			"hue": %d,
+			"bri": %d
+		}""" % (hue, bri)
+
+		response = requests.put(url + "/lights/1/state", data)
+		n += 1
+		#printDebug(response)
+
+def circular():
+	while True:
+		time.sleep(0.1)
+
+		#hue = random.uniform(0, 1) * 65534
+		hue = int((time.time()*30000 % 65535))
+
+		#print("x: " + str(x) + "\t" + "y: " + str(y))
+
+		data = """
+		{
+			"hue": %d
+		}""" % (hue)
+
+		response = requests.put(url + "/lights/1/state", data)
+		#printDebug(response)
+
+def printDebug(res):
+	print(response)
+	print(response.content)
+
+# Lightning mode
+randomHue()
+#circular()
